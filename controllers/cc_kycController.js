e167d7dc494115136ea31e6d41bcbd9fa752aacc
oyegbilepraise <oluwadhammieh@gmail.com>
2022-05-20 09:13:18 +0100

    feat: Added CC_KYC

--- controllers/cc_kycController.js
+++ controllers/cc_kycController.js
@@ -8,11 +8,14 @@ let stage = 0;
 
 const customer_kyc = async (req, res) => {
   const { phoneNumber, response } = req.body;
+
+  let newPhone = "0" + phoneNumber.substr(-10)
+
   try {
     if (step == 0 && stage == 0 && response.trim().toLowerCase() == "cc-kyc") {
       let fetch = await axios.post(
         "https://mobile.creditclan.com/api/v3/customer/check/details",
-        { phone: phoneNumber },
+        { phone: newPhone },
         {
           headers: {
             "x-api-key":
