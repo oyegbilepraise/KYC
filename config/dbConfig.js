fb3fd96ecd9b8584ae07816f6b9c759e61c650b1
oyegbilepraise <oluwadhammieh@gmail.com>
2022-05-23 11:37:41 +0100

    feat: Added CC_KYC

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,17 +1,17 @@
 require('dotenv').config()
 
 module.exports = {
-    // HOST: process.env.DATABASE_HOST,
-    // USER: process.env.DATABASE_USERNAME,
-    // PASSWORD: process.env.DATABASE_PASSWORD,
-    // DB: process.env.DATABASE_NAME,
-    // dialect: 'mysql',
-
-    HOST: 'localhost',
-    USER: 'root',
-    PASSWORD: '',
-    DB: 'customer_kyc',
+    HOST: process.env.DATABASE_HOST,
+    USER: process.env.DATABASE_USERNAME,
+    PASSWORD: process.env.DATABASE_PASSWORD,
+    DB: process.env.DATABASE_NAME,
     dialect: 'mysql',
+
+    // HOST: 'localhost',
+    // USER: 'root',
+    // PASSWORD: '',
+    // DB: 'customer_kyc',
+    // dialect: 'mysql',
 
     pool: {
         max: 5,
