0aea652cd70aa7859c63e6aee31a466d6a32509e
Oyegbile Praise <oluwadhammieh@gmail.com>
2023-01-07 11:59:42 +0100

    feat: Added Bolt Models

--- models/Agent.js
+++ models/Agent.js
@@ -1,9 +1,15 @@
+const { Sequelize } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   const AGENT = sequelize.define("ercan_agent", {
-        phoneNumber: {type: DataTypes.STRING, required: false},
-        keyword: {type: DataTypes.STRING, required: false},
-        name: {type: DataTypes.STRING, required: false},
-        referal_phone: {type: DataTypes.STRING, required: false}
+    id: {
+      type: Sequelize.UUID,
+      defaultValue: Sequelize.UUIDV4,
+      primaryKey: true,
+    },
+    phoneNumber: { type: DataTypes.STRING, required: false },
+    keyword: { type: DataTypes.STRING, required: false },
+    name: { type: DataTypes.STRING, required: false },
+    referal_phone: { type: DataTypes.STRING, required: false }
   });
 
   return AGENT;
